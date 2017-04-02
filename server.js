042925a3eacb011ba519fb05bac26396947c8c91
victor alves abreu <victoralvesabreu@outlook.com>
2017-04-02 01:54:50 -0300

    Add suport to MongoDB

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 var htpp = require('http');
 var app = require('./config/express')();
+require("./config/database.js")("mongodb://localhost/bdTeste");
 
 htpp.createServer(app).listen(app.get("port"), function(){
   console.log("Express Server escutando na prota " + app.get("port"));
